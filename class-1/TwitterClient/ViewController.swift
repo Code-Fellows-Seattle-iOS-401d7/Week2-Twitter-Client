d0a9df2ad52ffd0d23d622a819c018f769b25cd9
Corey Malek <coreymalek@Coreys-MacBook-Pro.local>
2016-10-18 13:46:20 -0700

    please work

---
+++ class-1/TwitterClient/ViewController.swift
@@ -0,0 +1,132 @@
+//
+//  ViewController.swift
+//  TwitterClient
+//
+//  Created by Corey Malek on 10/17/16.
+//  Copyright © 2016 Corey Malek. All rights reserved.
+//
+
+import UIKit
+
+
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var allTweets = [Tweet]() {
+        didSet {
+            tableView.reloadData()
+        }
+    }
+    
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        //instatiating UITableViewDelegate on line 56 allows us to write tableView.delegate on line 30. Otherwise it wouldn't know what it is a delegate of and return an error.
+        
+    }
+    
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        update()
+
+    }
+    
+    func update(){
+        API.shared.getTweets { (tweets) in
+            if tweets != nil {
+                OperationQueue.main.addOperation {
+                    self.allTweets = tweets!
+                }
+            }
+        }
+    }
+
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return allTweets.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TweetCell", for: indexPath)
+        
+        let currentTweet = self.allTweets[indexPath.row]
+        
+        cell.textLabel?.text = currentTweet.text
+        
+        return cell
+        
+    }
+    
+}
+// From code review //
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(indexPath.row)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// code challenge 
+
+var animalsArray = ["Dog", "Cat", "Pig", "Cow"]
+
+func reverseArray: [String] {
+     return animalsArray.reversed()
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
